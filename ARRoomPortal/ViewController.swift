881c0dd62c482ee4e1c722a0707a8efc017e3afa
JeremyPerezSwift <jeremyperez34000@gmail.com>
2020-08-02 09:40:59 +0200

    Initial Commit

---
+++ ARRoomPortal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ARRoomPortal
+//
+//  Created by Jérémy Perez on 02/08/2020.
+//  Copyright © 2020 Jérémy Perez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
